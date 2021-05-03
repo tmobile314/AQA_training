34a7c5c374132c0ef18f93af66fb9855e4942a40
tmobile314 <tmobl@yandex.ru>
2021-05-03 22:12:38 +0300

    Added a constructor to the "Point" class

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,13 +5,13 @@ public class MyFirstProgram {
   public static void main(String[] args) {
 
     // System.out.println("Hello, World!");
-    Point p1 = new Point();
-    p1.x = 5;
-    p1.y = 1;
+    Point p1 = new Point(7, 9);
+    //p1.x = 5;
+    //p1.y = 1;
 
-    Point p2 = new Point();
-    p2.x = 1;
-    p2.y = 4;
+    Point p2 = new Point(10, 5);
+    //p2.x = 1;
+    //p2.y = 4;
 
     System.out.println("Расстояние между точкой с координатами (" + p1.x + "; " + p1.y + ") и точкой с координатами (" + p2.x + "; " + p2.y + ") равно: " + distance(p1, p2));
 
