e33d8f6c85ed65af9926821fc547e6f054f31dc2
tmobile314 <tmobl@yandex.ru>
2021-05-03 22:50:25 +0300

    The program was redesigned/ Added method "distance" to the "Point" class

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,24 +5,11 @@ public class MyFirstProgram {
   public static void main(String[] args) {
 
     // System.out.println("Hello, World!");
-    Point p1 = new Point(7, 9);
-    //p1.x = 5;
-    //p1.y = 1;
 
-    Point p2 = new Point(10, 5);
-    //p2.x = 1;
-    //p2.y = 4;
+    Point p = new Point(4, 8, 7, 6);
 
-    System.out.println("Расстояние между точкой с координатами (" + p1.x + "; " + p1.y + ") и точкой с координатами (" + p2.x + "; " + p2.y + ") равно: " + distance(p1, p2));
+    System.out.println("Расстояние между точкой с координатами (" + p.x1 + "; " + p.y1 + ") и точкой с координатами (" + p.x2 + "; " + p.y2 + ") равно: " + p.distance());
 
   }
-    public static double distance(Point p1, Point p2){
-      double a = p1.x - p2.x;
-      double b = p1.y - p2.y;
-      double c = a * a + b * b;
-
-      return Math.sqrt(c) ;
-    }
-
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,12 +1,22 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  public double x;
-  public double y;
+  public double x1, x2;
+  public double y1, y2;
 
-  public Point(double x, double y){
-    this.x = x;
-    this.y = y;
+  public Point(double x1, double y1, double x2, double y2){
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+
+  public double distance(){
+    double a = this.x1 - this.x2;
+    double b = this.y1 - this.y2;
+    double c = a * a + b * b;
+
+    return Math.sqrt(c) ;
   }
 
 }
